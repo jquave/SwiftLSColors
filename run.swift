1e8dfc00c3f8c42e2c57cd776a29153a3960250d
Jameson Quave <jquave@gmail.com>
2014-06-15 12:42:39 -0500

    Colors

--- run.swift
+++ run.swift
@@ -2,14 +2,37 @@
 
 import Foundation
 
+let C_RED     = "\x1b[31m"
+let C_GREEN   = "\x1b[32m"
+let C_YELLOW  = "\x1b[33m"
+let C_BLUE    = "\x1b[34m"
+let C_MAGENTA = "\x1b[35m"
+let C_CYAN    = "\x1b[36m"
+let C_RESET   = "\x1b[0m"
+
+
 let fileManager = NSFileManager.defaultManager()
 
 let path = "./"
 let contents = fileManager.directoryContentsAtPath(path) as String[]
 
-for item: String in contents {
-  var isDirectory: ObjCBool = false
-  let fullPath = "\(path)\(item)"
-  fileManager.fileExistsAtPath(fullPath, isDirectory: &isDirectory)
-  println("\(fullPath): \(isDirectory)")
+func setColor(color: String) {
+  print(color)
 }
+
+for item: String in contents {
+  var isDirectoryOC: ObjCBool = false
+  let fullPath = "\(path)\(item)"
+  fileManager.fileExistsAtPath(fullPath, isDirectory: &isDirectoryOC)
+  let isDirectory = isDirectoryOC as Bool
+  if isDirectory {
+    setColor(C_RED)
+  }
+  else {
+    setColor(C_RESET)
+  }
+
+  println(item)
+
+}
+
